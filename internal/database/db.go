7defed5ed5722a163c784315ab46af926c494734
GlebTFD <glebiv2011@gmail.com>
2025-02-02 19:21:51 +0300

    db connect start
    start implementing connection to postgresql db

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func ConnectDB() {
+	connstr := fmt.Sprintf(
+		"postgres://%s:%s@%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_NAME"),
+	)
+
+	conn, err := pgx.Connect(context.Background(), connstr)
+}
